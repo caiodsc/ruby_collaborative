50dba8de6aa9c3ad9f080a26e742793431e2db36
Caio <caio.dscamara@gmail.com>
2018-07-05 20:12:32 -0400

    Euclidiana

--- test.rb
+++ test.rb
@@ -4,20 +4,14 @@ require_relative 'database'
 
 def euclidiana (usuario1, usuario2)
   ratings = Test.new.ratings
-  i = false
-    ratings[usuario1].each do |item|
-      if ratings[usuario2].include? item
-        i = true
-        break
-      end
-    end
-    soma = 0
-    #return 0 unless i
-    films = (ratings[usuario1].keys & ratings[usuario2].keys)
-    films.each do |film|
-      soma += 1 if ratings[usuario1][film] and ratings[usuario2][film]
-    end
-    soma.fdiv films.size
+  soma = 0
+  films = (ratings[usuario1].keys & ratings[usuario2].keys)
+  films.each do |film|
+    soma += 1 if ratings[usuario1][film] and ratings[usuario2][film]
+  end
+  total = films.size
+  return soma.fdiv total if total > 0
+  0
 end
 
 p euclidiana('Ana', 'Marcos')
