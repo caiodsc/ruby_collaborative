0c65c772f572e750235568fa92af200078ba95aa
Caio <caio.dscamara@gmail.com>
2018-07-05 22:37:58 -0400

    Get Recomendaçao

--- test.rb
+++ test.rb
@@ -19,5 +19,32 @@ def getSimilares(usuario)
     ratings.map {|k,v| euclidiana(usuario, k) unless k == usuario}.compact!.sort.reverse
 end
 
-p euclidiana('Ana', 'Marcos')
-p getSimilares('Ana')
+def getRecomendacoes(usuario)
+  ratings = Test.new.ratings
+  totais = {}
+  somaSimilaridade = {}
+  ratings.each do |outro|
+    next if outro == usuario
+    #p usuario, outro.first
+    similaridade = euclidiana(usuario, outro.first)
+    next if similaridade <= 0
+    ratings[outro.first].each do |item|
+      unless ratings[usuario].include? item
+        totais[item.first] = 0
+        totais[item.first] += ratings[outro.first][item.first] && 1 || 0 * similaridade
+        somaSimilaridade[item.first] = 0
+        somaSimilaridade[item.first] += similaridade
+      end
+    end
+  end
+  rankings = totais.map {|k,v| {:nome => k, :nota => v/somaSimilaridade[k]}}
+  rankings.sort_by {|hsh| -hsh[:nota]}
+  #p rankings.sort_by {|a,b| a[0] <=> b[0]}
+end
+
+
+
+#p euclidiana('Ana', 'Marcos')
+#p getSimilares('Ana')
+#getRecomendacoes('Ana')
+#p false && 1 || 0
